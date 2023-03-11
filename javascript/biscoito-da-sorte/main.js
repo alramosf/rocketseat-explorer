ad96a4788bb09b1f519540f8f97fee2f00165165
alramosf <106263609+alramosf@users.noreply.github.com>
2023-03-11 17:47:25 -0300

    projeto iniciado

---
+++ javascript/biscoito-da-sorte/main.js
@@ -0,0 +1,19 @@
+/* Variaveis */
+const closedCookie = document.querySelector(".closedCookie")
+const openedCookie = document.querySelector(".openedCookie")
+
+const btnAnotherCookie = document.querySelector(".btnAnotherCookie")
+
+
+/* Eventos */
+
+
+
+/* Funções */
+
+
+function toggleScreen() {
+    closedCookie.classList.toggle("hide")
+    openedCookie.classList.toggle("hide")
+}
+
