3b7cd35d9af8c1e0c64915f4b3c942aae8c0d95c
alramosf <106263609+alramosf@users.noreply.github.com>
2023-03-14 00:03:08 -0300

    update frases

--- javascript/biscoito-da-sorte/main.js
+++ javascript/biscoito-da-sorte/main.js
@@ -5,22 +5,17 @@ const openedCookie = document.querySelector(".openedCookie")
 const btnAnotherCookie = document.querySelector(".btnAnotherCookie")
 
 const frases = [
-    "A persistência é o caminho do êxito.",
-    "A sabedoria é a única riqueza que os tiranos não podem expropriar.",
-    "A vida é uma peça de teatro que não permite ensaios. Por isso, cante, chore, dance, ria e viva intensamente, antes que a cortina se feche e a peça termine sem aplausos.",
-    "Nossos fracassos, às vezes, são mais frutíferos do que os êxitos.",
-    "O sucesso é ir de fracasso em fracasso sem perder entusiasmo.",
-    "Não crie limites para si mesmo. Você deve ir tão longe quanto sua mente permitir. O que você mais quer pode ser conquistado.",
+    "O sucesso é a soma de pequenos esforços repetidos dia após dia.",
+    "A felicidade é uma escolha, faça-a todos os dias.",
+    "Acredite em si mesmo e no seu potencial.",
+    "Grandes realizações exigem grande esforço.",
+    "As oportunidades estão sempre ao seu redor, basta estar atento.",
+    "A paciência é uma virtude, e pode trazer grandes recompensas.",
+    "Aprenda com seus erros e cresça com suas experiências.",
+    "A vida é um presente, aprecie cada momento.",
+    "A persistência é a chave para o sucesso.",
+    "Você é capaz de alcançar tudo o que deseja, basta acreditar e agir.",
     "Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.",
-    "A sabedoria é a irmã mais velha da inspiração.",
-    "Só sei que nada sei.",
-    "Conhece-te a ti mesmo.",
-    "A educação é a arma mais poderosa que você pode usar para mudar o mundo.",
-    "A verdadeira sabedoria está em reconhecer a própria ignorância.",
-    "Não basta adquirir sabedoria, é preciso usá-la.",
-    "A mente que se abre a uma nova ideia jamais voltará ao seu tamanho original.",
-    "A experiência é o nome que damos aos nossos erros.",
-    "O conhecimento é poder.",
 ];
 
 /* Eventos */
