0e8b173c8b4ce0afc6c4d7b6902e1e4c71fc9115
alramosf <106263609+alramosf@users.noreply.github.com>
2023-03-26 14:48:59 -0300

    script

---
+++ javascript/timer/script.js
@@ -0,0 +1,13 @@
+// DOM
+// Document Object Model
+
+
+const play = document.querySelector('.play')
+const pause = document.querySelector('.pause')
+const stop = document.querySelector('.stop')
+const set = document.querySelector('.set')
+
+const soundOn = document.querySelector('.sound-on')
+const soundOff = document.querySelector('.sound-off')
+
+play.classList.add('hide')
