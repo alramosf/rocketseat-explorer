35986c90b622115a832988ec82592968955ad7a3
alramosf <106263609+alramosf@users.noreply.github.com>
2023-06-06 17:43:48 -0300

    inicio do projeto

---
+++ javascript/github-favorites/js/favorites.js
@@ -0,0 +1,106 @@
+// classe que vai conter a lógica dos dados
+// como os dados serão estruturados
+
+export class Favorites {
+    constructor(root){
+        this.root = document.querySelector(root);
+        this.load()
+    }
+
+    load(){
+        this.entries = [{
+            login: 'alramosf',
+            name: 'Alexandre Ramos',
+            public_repos: '10',
+            followers: '2',
+        },
+        {
+            login: 'maykbrito',
+            name: 'Mayk Brito',
+            public_repos: '100',
+            followers: '12000',
+        }
+        ]
+
+    }
+
+    delete(user){
+        //Higher-order funcions (map, filter, reduce...)
+        const filteredEntries = this.entries.filter(entry => {
+        entry.login !== user.login
+        })
+        console.log(filteredEntries)
+    }
+}
+
+// classe que vai criar a visualização e eventos do HTML
+
+export class FavoritesView extends Favorites {
+    constructor(root){
+        super(root)
+
+        this.tbody = this.root.querySelector('table tbody')
+
+        this.update()
+    }
+
+    update(){
+        this.removeAllTr()
+
+        this.entries.forEach(user => {
+            const row = this.createRow()
+
+            row.querySelector('.user img').src =`https://github.com/${user.login}.png`
+            row.querySelector('.user img').alt = `Imagem de ${user.name}`
+
+            row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user span').textContent = user.login
+            row.querySelector('.repositories').textContent = user.public_repos
+            row.querySelector('.followers').textContent = user.followers
+
+
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+                if(isOk){
+                    this.delete(user)
+                }
+            }
+
+            this.tbody.append(row)
+        })
+
+}
+
+    createRow(){
+
+        const tr = document.createElement('tr')
+
+        tr.innerHTML = `
+            <td class="user">
+                <img src="https://github.com/alramosf.png" alt="">
+                <a href="https://github.com/alramosf" target="bl
+                ">
+                    <p>Alexandre Ramos</p>
+                    <span>alramosf</span>
+                </a>
+            </td>
+            <td class="repositories">
+                10
+            </td>
+            <td class="followers">
+                2
+            </td>
+            <td><button class="remove">&times;</button></td>
+        `
+
+        return tr
+    }
+
+    removeAllTr(){
+        
+        this.tbody.querySelectorAll('tr')
+        .forEach((tr) => {
+            tr.remove()
+        })
+    }
+}
