da1f5abbf892be27c10cf6ea2f466f1784e31429
alramosf <106263609+alramosf@users.noreply.github.com>
2023-06-09 13:11:22 -0300

    projeto finalizado

--- javascript/github-favorites/js/favorites.js
+++ javascript/github-favorites/js/favorites.js
@@ -1,17 +1,4 @@
-export class GithubUser {
-    static search(username) {
-        const endpoint = `https://api.github.com/users/${username}`
-
-        return fetch(endpoint)
-        .then(data => data.json())
-        .then(({login, name, public_repos, followers}) => ({
-            login,
-            name,
-            public_repos,
-            followers,
-        }))
-    }
-}
+import { GithubUser } from "./GithubUser.js";
 
 // classe que vai conter a lógica dos dados
 // como os dados serão estruturados
@@ -28,14 +15,28 @@ export class Favorites {
 
     }
 
+    save(){
+        localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+    }
+
     async add(username){
         try{
+
+            const userExists = this.entries.find(entry => entry.login === username)
+
+            if(userExists) {
+                throw new Error('Usuário já cadastrado.')
+            }
+
             const user = await GithubUser.search(username)
             if (user.login === undefined){
                 throw new Error('Usuário não encontrado!')
             }
 
             this.entries = [user, ...this.entries]
+            this.update()
+            this.save()
+
         } catch(error){
             alert(error.message)
         }
@@ -50,6 +51,7 @@ export class Favorites {
 
         this.entries = filteredEntries
         this.update()
+        this.save()
         
     }
 }
@@ -83,7 +85,7 @@ export class FavoritesView extends Favorites {
 
             row.querySelector('.user img').src =`https://github.com/${user.login}.png`
             row.querySelector('.user img').alt = `Imagem de ${user.name}`
-
+            row.querySelector('.user a').href = `https://github.com/${user.login}`
             row.querySelector('.user p').textContent = user.name
             row.querySelector('.user span').textContent = user.login
             row.querySelector('.repositories').textContent = user.public_repos
