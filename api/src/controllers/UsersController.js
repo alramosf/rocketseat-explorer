35926fe3c9481dd351bdf783808e72e5694adf51
alramosf <106263609+alramosf@users.noreply.github.com>
2023-06-13 15:15:14 -0300

    update

---
+++ api/src/controllers/UsersController.js
@@ -0,0 +1,8 @@
+class UsersController {
+    /*
+    * Index - GET para listar vários registros
+    
+    */
+}
+
+module.exports = UsersController;
--- api/src/server.js
+++ api/src/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
+const routes = require("./routes")
+
+
 const app = express();
+app.use(express.json())
 
-app.get("/message", (request, response) => {
-    response.send("Hello World!")
-})
-
+app.use(routes)
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
