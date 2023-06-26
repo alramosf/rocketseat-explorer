a2fa7d610fb0542cfd6e0d5c9eeca94f51ce16cd
alramosf <106263609+alramosf@users.noreply.github.com>
2023-06-25 21:53:09 -0300

    up

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require('bcryptjs')
+const { hash, compare } = require('bcryptjs')
 const AppError = require("../utils/AppError")
 
 const sqliteConnection = require('../database/sqlite')
@@ -32,7 +32,7 @@ class UsersController {
     } 
 
     async update(request, response){
-        const { name, email } = request.body;
+        const { name, email, password, old_password } = request.body;
         const { id } = request.params;
 
         const database = await sqliteConnection();
@@ -48,16 +48,32 @@ class UsersController {
             throw new AppError('E-mail informado ja está em uso.');
         }
 
-        user.name = name;
-        user.email = email;
+        user.name = name ?? user.name;
+        user.email = email ?? user.email;
+
+
+        if(password && !old_password){
+            throw new AppError ("Você precisa informar a senha antiga!")
+        }
+
+        if (password && old_password){
+            const checkOldPassowrd =  await compare(old_password, user.password);
+
+            if(!checkOldPassowrd){
+                throw new AppError ('Senha atual incorreta!');
+            }
+
+            user.password = await hash(password, 8)
+        }
 
         await database.run(`
         UPDATE users SET
         name = ?,
         email = ?,
-        updated_at = ?
+        password = ?,
+        updated_at = DATETIME('now')
         WHERE id = ?`,
-        [user.name, user.email, new Date(), id]
+        [user.name, user.email, user.password, id]
         );
 
         return response.status(200).json()
