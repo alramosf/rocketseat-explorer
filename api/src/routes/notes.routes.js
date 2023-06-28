6f4b9dc9041999755a9c3badc8fc8d52e2e00097
alramosf <106263609+alramosf@users.noreply.github.com>
2023-06-27 23:17:52 -0300

    update
    Adicionado consulta, adicionado filtros, junção de duas tabelas na busca de informação

--- api/src/routes/notes.routes.js
+++ api/src/routes/notes.routes.js
@@ -6,7 +6,10 @@ const notesRoutes = Router();
 
 const notesController = new NotesController();
 
+notesRoutes.get("/", notesController.index)
 notesRoutes.post("/:user_id", notesController.create)
+notesRoutes.get("/:id", notesController.show)
+notesRoutes.delete("/:id", notesController.delete)
 
 
 module.exports = notesRoutes;
