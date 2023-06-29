35d785601eafe1df105c809d9a589fca5ef12d7c
alramosf <106263609+alramosf@users.noreply.github.com>
2023-06-29 07:40:50 -0300

    update

--- api/src/controllers/NotesController.js
+++ api/src/controllers/NotesController.js
@@ -83,8 +83,16 @@ class NotesController{
 
         }
 
+        const userTags = await knex("tags").where({ user_id })
+        const notesWithTags = notes.map(note => {
+            const noteTags = userTags.filter(tag => tag.note_id === note.id);
+            return {
+                ...note,
+                tags: noteTags
+            }
+        });
 
-        return response.json(notes)
+        return response.json(notesWithTags)
     }
 };
 
